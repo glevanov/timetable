6f187ceeec2a4670646036ff8cf20c98c2aeb9e9
Grigoriy Levanov <glevanov@gmail.com>
2018-08-25 15:58:48 +0300

    Adds departures data

--- app/js/data.js
+++ app/js/data.js
@@ -147,9 +147,302 @@ export default {
       destination: `ИРАКЛИОН`,
       id: `A3 490`,
       status: `Ожидается по расписанию`
+    },
+    {
+      time: `11:15`,
+      destination: `СИМФЕРОПОЛЬ`,
+      id: `GH 264`,
+      status: `Ожидается по расписанию`
+    },
+    {
+      time: `11:15`,
+      destination: `ЛИПЕЦК`,
+      id: `S7 248`,
+      status: `Ожидается по расписанию`
+    },
+    {
+      time: `11:25`,
+      destination: `ДАЛАМАН`,
+      id: `WZ 9630`,
+      status: `Ожидается по расписанию`
+    },
+    {
+      time: `11:25`,
+      destination: `ДАЛАМАН`,
+      id: `U6 2898`,
+      status: `Ожидается по расписанию`
+    },
+    {
+      time: `11:25`,
+      destination: `СТАМБУЛ`,
+      id: `PC 388`,
+      status: `Ожидается по расписанию`
+    },
+    {
+      time: `11:30`,
+      destination: `ВОРОНЕЖ`,
+      id: `S7 232`,
+      status: `Ожидается по расписанию`
+    },
+    {
+      time: `11:30`,
+      destination: `ГАЗИПАША`,
+      id: `ZF 7794`,
+      status: `Ожидается по расписанию`
+    },
+    {
+      time: `11:35`,
+      destination: `САНКТ-ПЕТЕРБУРГ`,
+      id: `S7 48`,
+      status: `Ожидается по расписанию`
+    },
+    {
+      time: `11:40`,
+      destination: `АНТАЛЬЯ`,
+      id: `ZF 884`,
+      status: `Ожидается по расписанию`
+    },
+    {
+      time: `11:40`,
+      destination: `КАЗАНЬ`,
+      id: `S7 62`,
+      status: `Ожидается по расписанию`
+    },
+    {
+      time: `11:45`,
+      destination: `ИРКУТСК`,
+      id: `6R 566`,
+      status: `Ожидается по расписанию`
+    },
+    {
+      time: `11:50`,
+      destination: `АНТАЛЬЯ`,
+      id: `U6 3002`,
+      status: `Ожидается по расписанию`
+    },
+    {
+      time: `11:50`,
+      destination: `ЯКУТСК`,
+      id: `GH 110`,
+      status: `Ожидается по расписанию`
     }
   ],
   departures: [
-
+    {
+      time: `11:00`,
+      destination: `МИНСК`,
+      id: `B2 958`,
+      status: `Вылетел`
+    },
+    {
+      time: `11:00`,
+      destination: `ТИВАТ`,
+      id: `S7 625`,
+      status: `Задержан`
+    },
+    {
+      time: `11:05`,
+      destination: `ТАШКЕНТ`,
+      id: `S7 625`,
+      status: `Вылетел`
+    },
+    {
+      time: `11:05`,
+      destination: `РИМ`,
+      id: `S7 625`,
+      status: `Вылетел`
+    },
+    {
+      time: `11:15`,
+      destination: `ОШ`,
+      id: `YK 966`,
+      status: `Задержан`
+    },
+    {
+      time: `11:15`,
+      destination: `НОВЫЙ УРЕНГОЙ`,
+      id: `GH 423`,
+      status: `Задержан`
+    },
+    {
+      time: `11:15`,
+      destination: `НОВОСИБИРСК`,
+      id: `GH 179`,
+      status: `Вылетел`
+    },
+    {
+      time: `11:30`,
+      destination: `ПАФОС`,
+      id: `GH 179`,
+      status: `Вылетел`
+    },
+    {
+      time: `11:30`,
+      destination: `САНКТ-ПЕТЕРБУРГ`,
+      id: `U6 79`,
+      status: `Задержан`
+    },
+    {
+      time: `11:35`,
+      destination: `НЕАПОЛЬ`,
+      id: `S7 605`,
+      status: `Регистрация`
+    },
+    {
+      time: `11:35`,
+      destination: `ЛАРНАКА`,
+      id: `GH 603`,
+      status: `Регистрация`
+    },
+    {
+      time: `11:40`,
+      destination: `БИШКЕК`,
+      id: `YK 884`,
+      status: `Регистрация`
+    },
+    {
+      time: `11:40`,
+      destination: `СОЧИ`,
+      id: `WZ 5551`,
+      status: `Регистрация`
+    },
+    {
+      time: `11:55`,
+      destination: `ВЕРОНА`,
+      id: `IG 9997`,
+      status: `Регистрация`
+    },
+    {
+      time: `11:55`,
+      destination: `РОДОС`,
+      id: `A3 3575`,
+      status: `Регистрация закрыта`
+    },
+    {
+      time: `11:55`,
+      destination: `УФА`,
+      id: `A3 3575`,
+      status: `Регистрация`
+    },
+    {
+      time: `12:00`,
+      destination: `КЕРКИРА`,
+      id: `A3 479`,
+      status: `Регистрация закрыта`
+    },
+    {
+      time: `12:00`,
+      destination: `КАЛИНИНГРАД`,
+      id: `U6 627`,
+      status: `Идет посадка`
+    },
+    {
+      time: `12:05`,
+      destination: `ДУШАНБЕ`,
+      id: `SZ 202`,
+      status: `Идет посадка`
+    },
+    {
+      time: `12:05`,
+      destination: `КРАСНОДАР`,
+      id: `QR 5761`,
+      status: `Идет посадка`
+    },
+    {
+      time: `12:10`,
+      destination: `ТИВАТ`,
+      id: `YM 4611`,
+      status: `По расписанию`
+    },
+    {
+      time: `12:15`,
+      destination: `ОМСК`,
+      id: `IO 202`,
+      status: `Задержан`
+    },
+    {
+      time: `12:15`,
+      destination: `СТАМБУЛ`,
+      id: `PC 389`,
+      status: `По расписанию`
+    },
+    {
+      time: `12:25`,
+      destination: `АНАПА`,
+      id: `U6 157`,
+      status: `По расписанию`
+    },
+    {
+      time: `12:25`,
+      destination: `ФРАНКФУРТ`,
+      id: `S7 3355`,
+      status: `По расписанию`
+    },
+    {
+      time: `12:25`,
+      destination: `БАКУ`,
+      id: `S7 3355`,
+      status: `По расписанию`
+    },
+    {
+      time: `12:25`,
+      destination: `БУРГАС`,
+      id: `S7 923`,
+      status: `По расписанию`
+    },
+    {
+      time: `12:30`,
+      destination: `САМАРА`,
+      id: `TP 8146`,
+      status: `По расписанию`
+    },
+    {
+      time: `12:30`,
+      destination: `АСТРАХАНЬ`,
+      id: `EY 4341`,
+      status: `По расписанию`
+    },
+    {
+      time: `12:40`,
+      destination: `ЕРЕВАН`,
+      id: `GH 905`,
+      status: `По расписанию`
+    },
+    {
+      time: `12:45`,
+      destination: `ДЮССЕЛЬДОРФ`,
+      id: `S7 991`,
+      status: `По расписанию`
+    },
+    {
+      time: `12:45`,
+      destination: `БЕРЛИН`,
+      id: `S7 895`,
+      status: `По расписанию`
+    },
+    {
+      time: `12:50`,
+      destination: `АБУ ДАБИ`,
+      id: `EY 068`,
+      status: `По расписанию`
+    },
+    {
+      time: `12:55`,
+      destination: `САНКТ-ПЕТЕРБУРГ`,
+      id: `S7 39`,
+      status: `По расписанию`
+    },
+    {
+      time: `12:55`,
+      destination: `ДОХА`,
+      id: `QR 234`,
+      status: `По расписанию`
+    },
+    {
+      time: `12:55`,
+      destination: `ДЖЕРБА`,
+      id: `ZF 7711`,
+      status: `По расписанию`
+    }
   ]
 };
--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,4 @@
 import data from './data.js';
 
 console.log(data.arrivals);
+console.log(data.departures);
