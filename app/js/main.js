03b0207755f8d3792ea4ecb66a0436e95b4c698d
Grigoriy Levanov <glevanov@gmail.com>
2018-08-26 15:57:57 +0300

    Adds stub event handlers for controls

--- app/js/main.js
+++ app/js/main.js
@@ -3,3 +3,21 @@ import { updateFlightsTable, updateStatus, updateCurrentData } from './util.js'
 updateCurrentData()
 updateStatus()
 updateFlightsTable()
+
+const searchForm = document.querySelector(`.search`);
+searchForm.addEventListener(`submit`, (evt) => {
+  evt.preventDefault();
+  console.log(`I submit!`);
+});
+
+const directionButton = document.querySelector(`.filter__direction`);
+directionButton.addEventListener(`click`, (evt) => {
+  evt.preventDefault();
+  console.log(`I direct!`);
+});
+
+const delayedButton = document.querySelector(`.filter__delayed`);
+delayedButton.addEventListener(`click`, (evt) => {
+  evt.preventDefault();
+  console.log(`I delay!`);
+});
