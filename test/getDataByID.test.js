27606c189f12aac1be0e90acdd48a5db4db2e14d
Grigoriy Levanov <glevanov@gmail.com>
2018-09-23 15:25:28 +0300

    Rewrites tests to Jest

--- test/getDataByID.test.js
+++ test/getDataByID.test.js
@@ -1,17 +1,11 @@
-import { assert } from "chai";
 import getDataByID from "../src/js/getDataByID";
 
-const sampleData = [{ id: `1` }, { id: `2` }, { id: `2` }];
-const sampleResult = [{ id: `2` }, { id: `2` }];
+test("it returns an array", () => {
+  expect(Array.isArray(getDataByID("id", []))).toBe(true);
+});
+test("it returns items with correct id", () => {
+  const sampleData = [{ id: `1` }, { id: `2` }, { id: `2` }];
+  const sampleResult = [{ id: `2` }, { id: `2` }];
 
-describe("getDataByID", () => {
-  it("returns an array", () => {
-    assert.isArray(getDataByID("id", []));
-  });
-  it("returns items with correct id", () => {
-    assert.equal(
-      JSON.toString(getDataByID("2", sampleData)),
-      JSON.toString(sampleResult)
-    );
-  });
+  expect(getDataByID("2", sampleData)).toEqual(sampleResult);
 });
